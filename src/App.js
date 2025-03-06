c759687fd6351f83f3ee8d6c91b778b90d554d31
Ryan S Werner <ryan@rswerner.com>
2025-03-05 18:46:15 -0600

    First Design

--- src/App.js
+++ src/App.js
@@ -1,19 +1,97 @@
-import logo from './logo.svg';
+
+import { Card, Col, Container, Nav, NavDropdown, Navbar, Row } from 'react-bootstrap';
 import './App.css';
 import { useEffect, useState } from 'react';
-import Blob from './Blob';
-import BlobGenerator from './BlobGenerator';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navigation from './Nav/Navigation';
 
 
 function App() {
 
+  const [mode, setMode] = useState('light')
+
+  useEffect(() => {
+    const html = document.getElementById('html')
+    if (mode === 'light') {
+      html.setAttribute('data-bs-theme','light')
+    } else {
+      html.setAttribute('data-bs-theme','dark')
+    }
+  }, [mode])
 
   return (
-    <div className="App">
-      <div className='lightbox'>
-        <BlobGenerator />
-      </div>
-      <p>hello world</p>
+    <div className="App" data-bs-theme={mode}>
+      <Navigation mode={mode} setMode={setMode} />
+      <video muted autoPlay loop width='100%' src='ryan-preview.mp4'></video>
+        
+        
+        <Card className='center-over-video p-3' style={{width: '500px'}}>
+          <Card.Img src='werner-nonoil.jpg'></Card.Img>
+          <Card.Body>
+            <Card.Title className=''><h1>Ryan S Werner</h1></Card.Title>
+            <Card.Text>Pharmacist | Web Developer | Corgi Enthusiast</Card.Text>
+          </Card.Body>
+        </Card>
+        <h1 className='mt-5'>About Me</h1>
+        <hr className='mx-5' />
+        
+        <Container className='pt-3'>
+          <Row>
+            <Col>
+            <Card>
+              <Card.Img src='walgreen.jpg'></Card.Img>
+              <Card.Body>
+                <Card.Title>Experience</Card.Title>
+                <Card.Text><p>I have an abundance of experience between pharmacy and information technology roles</p>
+                <div style={{textAlign: 'left'}}>
+                <ul>
+                  <li>Pharmacy Manager</li>
+                    <ul><li>Walgreens #09214</li></ul>
+                  <li>Sr. Student Team Lead</li>
+                    <ul><li>UW-Madison Division of Information Technology</li></ul>
+                </ul>
+
+                </div>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            </Col>
+            <Col><Card>
+              <Card.Img src='capitol.jpg'></Card.Img>
+              <Card.Body>
+                <Card.Title>Values</Card.Title>
+                <Card.Text><p>My mission is to help better lives through connecting healthcare and information technology systems</p>
+                <div style={{textAlign: 'left'}}>
+                <ul><li>Integrity</li>
+                <ul><li>I act standing on my own principles of what is right</li></ul>
+                <li>Honesty</li>
+                <ul><li>I am not afraid to admit when I am wrong</li></ul></ul>
+                </div>
+                </Card.Text>
+              </Card.Body>
+            </Card></Col>
+            <Col><Card>
+              <Card.Img src='bascom.jpg'></Card.Img>
+              <Card.Body>
+                <Card.Title>Education</Card.Title>
+                <Card.Text><p>I strive to learn and apply new information to my tasks</p>
+                <div style={{textAlign: 'left'}}>
+                <ul>
+                <li>University of Wisconsin - Madison</li>
+                <ul>
+                  <li>Doctor of Pharmacy (PharmD), 2023</li>
+                  <ul><li>Named option in Operations and Technology Management</li></ul>
+                  <li>B.S. Pharmaceutical Sciences, 2021</li>
+                  </ul>
+                  </ul>
+                </div>
+                </Card.Text>
+              </Card.Body>
+            </Card></Col>
+          </Row>
+        </Container>
+        <br />
+
     </div>
   );
 }
---
+++ src/Nav/Navigation.js
@@ -0,0 +1,28 @@
+import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
+
+
+export default function Navigation(props) {
+
+    return <Navbar data-bs-theme={props.mode} bg={props.mode}>
+        <Container>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">About</Nav.Link>
+            <NavDropdown title="Other Sites" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item onClick={() => props.setMode(props.mode === 'light' ? 'dark' : 'light')}>
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+        </Container>
+      </Navbar>
+}
